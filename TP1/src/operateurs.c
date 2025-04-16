d284d0315552a164f65b9ba65703a862f8649ee9
swrge <swrgingrage@gmail.com>
2025-04-16 15:12:05 +0200

    feat(TP1): operateurs.c

---
+++ TP1/src/operateurs.c
@@ -0,0 +1,18 @@
+// operateurs.c
+//
+// Écrivez un programme nommé opérateurs.c dans la fonction main() qui utilise deux variables a et b
+//
+
+#include <stdio.h>
+
+void main() {
+    int a = 16;
+    int b = 3;
+
+    printf("Addition: %d\n", a + b);
+    printf("Soustraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Modulo: %d\n", a % b);
+    printf("Est supérieur: %d\n", a > b);
+}
